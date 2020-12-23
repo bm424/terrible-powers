53908cbb7a2881df6a8c8880e2ed1ddbf9cd7cb6
Ben Martineau <benhmartineau@gmail.com>
2020-12-23 20:00:38 +0000

    Add tailwind to build and set up initial configuration

---
+++ app/tailwind.config.js
@@ -0,0 +1,30 @@
+const sans = [
+  'ui-sans-serif',
+  'system-ui',
+  '-apple-system',
+  'BlinkMacSystemFont',
+  '"Segoe UI"',
+  'Roboto',
+  '"Helvetica Neue"',
+  'Arial',
+  '"Noto Sans"',
+  'sans-serif',
+  '"Apple Color Emoji"',
+  '"Segoe UI Emoji"',
+  '"Segoe UI Symbol"',
+  '"Noto Color Emoji"',
+]
+
+const serif = [
+  'ui-serif', 'Georgia', 'Cambria', '"Times New Roman"', 'Times', 'serif'
+]
+
+module.exports = {
+  purge: [
+    './www/*.html',
+  ],
+  extend: {},
+  theme: {},
+  variants: {},
+  plugins: [],
+}
